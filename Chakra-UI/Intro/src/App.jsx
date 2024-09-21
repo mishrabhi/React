2c3db8593552c432a0286cdaf00a9f8f636436a9
mishrabhi <asmyselfabhishek00@gmail.com>
2024-09-21 12:57:12 +0530

    created Ui using chakra

--- Chakra-UI/Intro/src/App.jsx
+++ Chakra-UI/Intro/src/App.jsx
@@ -1,12 +1,29 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { Box, Button, Heading, Input, Text, VStack } from "@chakra-ui/react";
 
-function App() {
-  const [count, setCount] = useState(0);
+const App = () => {
+  return (
+    <Box p={5} maxW="600px" mx="auto" borderWidth="1px" borderRadius="1g">
+      <Heading as="h1" size="x1" mb={4}>
+        First UI using Chakra
+      </Heading>
 
-  return <></>;
-}
+      <Input placegolder="Enter Something ..." mb={4} />
+      <Button colorScheme="blue" mb={4}>
+        Submit
+      </Button>
+
+      <VStack
+        p={4}
+        borderWidth="1px"
+        borderRadius="1g"
+        spacing={4}
+        align="stretch"
+      >
+        <Text fontSize="md">This is a simple card build with Chakra UI</Text>
+        <Button colorScheme="teal">Click Me </Button>
+      </VStack>
+    </Box>
+  );
+};
 
 export default App;
